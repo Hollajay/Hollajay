52044f902761f2cd2fbfbabb34b994095ffca0fc
Hollajay <127396029+Hollajay@users.noreply.github.com>
2024-04-16 21:03:31 +0100

    my weather web

---
+++ app.js
@@ -0,0 +1,16 @@
+
+// function fetcher(){
+//     fetch( "https://ipgeolocation.abstractapi.com/v1/?api_key=d5c0aa6071fc4c4cab5d98bcd559979f&ip_address=102.88.43.143")
+//     .then((Response) =>{
+//        return Response.json()
+//     })
+//     .then((weather) =>{
+//        console.log(weather);
+
+ 
+//     }) 
+//     .catch((error) =>{
+//        console.error("fetching Error",error.message);
+//     })
+// } 
+// fetcher()
